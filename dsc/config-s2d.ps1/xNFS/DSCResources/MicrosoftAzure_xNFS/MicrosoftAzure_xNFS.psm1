dd06c696ff606c8b12560659aa3726b870ecbda1
Keith Mayer <keith@keithmayer.com>
2017-04-07 15:49:13 -0400

    Updated NFS resource to restart NFSService on each cluster node

--- dsc/config-s2d.ps1/xNFS/DSCResources/MicrosoftAzure_xNFS/MicrosoftAzure_xNFS.psm1
+++ dsc/config-s2d.ps1/xNFS/DSCResources/MicrosoftAzure_xNFS/MicrosoftAzure_xNFS.psm1
@@ -37,11 +37,17 @@ function Set-TargetResource
 
     )
  
+    # Add NFS Server role to Cluster
+    
     $Disks = Get-ClusterResource -ErrorAction SilentlyContinue | Where-Object Name -like "*${NFSName}_*"
 
     Add-ClusterFileServerRole -Storage $Disks.Name -Name $NFSName -StaticAddress $LBIPAddress -ErrorAction Stop -Verbose
 
+    # Make sure NFS Server role is active on this node
+
     Move-ClusterGroup -Name $NFSName -Node $env:COMPUTERNAME -ErrorAction Stop -Verbose
+
+    # Update IP Address Resource for NFS Group with Azure Load Balancer IP Address
 
     $ClusterNetworkName = "Cluster Network 1"
     $IPResourceName = "IP Address ${LBIPAddress}"
@@ -57,11 +63,15 @@ function Set-TargetResource
         "EnableDhcp"=0
         }
 
+    # Stop and Start Cluster Group so that IP Resource change takes effect
+    
     Stop-ClusterGroup -Name $NFSName -ErrorAction SilentlyContinue -Verbose
 
     Start-ClusterGroup -Name $NFSName -ErrorAction Stop -Verbose
 
     Start-Sleep -Seconds 60
+
+    # Create NFS shared folder and set initial permissions
 
     [system.io.directory]::CreateDirectory("F:\${ShareName}")
     
@@ -69,11 +79,21 @@ function Set-TargetResource
     
     Start-Process -FilePath "${env:SystemRoot}\System32\nfsfile.exe" -ArgumentList "/r m=777 F:\${ShareName}" -Wait
 
-    Set-NfsServerConfiguration -LeasePeriodSec 10 -GracePeriodSec 20
-
-    Restart-Service -Name NfsService
+    # Enable Auto-failback of Cluster Groups
 
     (Get-ClusterGroup) | Foreach-Object {$_.AutoFailbackType = 1}
+
+    # Set NFS Lease and Grace duration
+
+    Set-NfsServerConfiguration -LeasePeriodSec 10 -GracePeriodSec 20
+
+    # Restart NFS Service on each Cluster Node to make new Lease and Grace durations effective
+
+    Set-Item wsman:\localhost\Client\TrustedHosts -Value "<local>" -Force
+
+    Get-ClusterNode -Cluster . | Foreach-Object { Invoke-Command -ComputerName $_.Name -ScriptBlock { Restart-Service -Name NfsService } }
+
+    Set-Item wsman:\localhost\Client\TrustedHosts -Value "" -Force
 
 }
 
